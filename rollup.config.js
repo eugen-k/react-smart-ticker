bf705959638888930d4256b8163e6cef9498a56c
Eugen K <eugen.korolev@gmail.com>
2024-07-25 12:59:17 +0300

    fix: cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import typescript from 'rollup-plugin-typescript2'
 import postcssPresetEnv from 'postcss-preset-env'
 import autoprefixer from 'autoprefixer'
 import postcss from 'rollup-plugin-postcss'
+import dts from 'rollup-plugin-dts'
 
 export default [
   {
@@ -42,101 +43,21 @@ export default [
       terser()
     ],
     external: ['react', 'react-dom']
+  }, // TypeScript declaration files
+  {
+    input: {
+      index: 'src/index.ts',
+      SmartTicker: 'src/components/SmartTicker/index.tsx',
+      SmartTickerDraggable: 'src/components/SmartTickerDraggable/index.tsx'
+    },
+    output: [
+      {
+        dir: 'dist',
+        format: 'es',
+        entryFileNames: '[name].d.ts',
+        preserveModulesRoot: 'src'
+      }
+    ],
+    plugins: [dts()]
   }
-  /* {
-    input: {
-      index: 'src/index.ts'
-    },
-    output: [
-      {
-        dir: 'dist',
-        format: 'cjs',
-        entryFileNames: '[name].js'
-      },
-      {
-        dir: 'dist',
-        format: 'esm',
-        entryFileNames: '[name].esm.js'
-      }
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      postcss({
-        plugins: [postcssPresetEnv(), autoprefixer()],
-        extract: false,
-        modules: true,
-        use: ['sass']
-      }),
-      terser()
-    ],
-    external: ['react', 'react-dom']
-  },
-  {
-    input: {
-      SmartTicker: './src/components/SmartTicker/index.tsx'
-    },
-    output: [
-      {
-        dir: './dist',
-        format: 'cjs',
-        sourcemap: true,
-        entryFileNames: 'SmartTicker.js'
-      },
-      {
-        dir: './dist',
-        entryFileNames: 'SmartTicker.esm.js',
-        format: 'esm',
-        interop: 'compat',
-        banner: `"use client"`
-      }
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      postcss({
-        plugins: [postcssPresetEnv(), autoprefixer()],
-        extract: false,
-        modules: true,
-        use: ['sass']
-      }),
-      terser()
-    ],
-    external: ['react', 'react-dom']
-  },
-  {
-    input: {
-      SmartTickerDraggable: './src/components/SmartTickerDraggable/index.tsx'
-    },
-    output: [
-      {
-        dir: './dist',
-        format: 'cjs',
-        sourcemap: true,
-        entryFileNames: 'SmartTickerDraggable.js'
-      },
-      {
-        dir: './dist',
-        entryFileNames: 'SmartTickerDraggable.esm.js',
-        format: 'esm',
-        interop: 'compat',
-        banner: `"use client"`
-      }
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      postcss({
-        plugins: [postcssPresetEnv(), autoprefixer()],
-        extract: false,
-        modules: true,
-        use: ['sass']
-      }),
-      terser()
-    ],
-    external: ['react', 'react-dom']
-  } */
 ]
